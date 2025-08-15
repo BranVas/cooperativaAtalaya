77522fca258c6d3218858276f4775ef4c8afc28c
Brans <branswervasquez@gmail.com>
2025-08-15 14:21:28 -0500

    dashboard con esqueleto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,8 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+<x-dashboard.dashboard2>
+</x-dashboard.dashboard2>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
